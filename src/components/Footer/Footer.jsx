ce2985fe0289f5807fba78433e7cb6f17b99bad7
Michael LaFond <Mike24mn@gmail.com>
2024-05-29 15:04:50 -0500

    aifjasfidaf

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Footer() {
+    return (
+    <div>
+        <footer>
+        <h3>Have fun!</h3>
+        <p>Don't forget to mind your Ps and Qs!</p>
+        </footer>
+    </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+    return (
+    <div>
+      <header className="Header-main">
+        <h1>Prim Proper Props</h1>
+      </header>
+    </div>
+    )
+  }
+  
+  export default Header;
